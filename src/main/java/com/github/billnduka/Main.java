e0655c9f7472826933c23202d104e423b3491120
0bserver <billnduka01@gmail.com>
2025-02-25 23:07:59 +0100

    Learned more about java classes, created the ball on the canvas. Next Learn how to update its position

--- src/main/java/com/github/billnduka/Main.java
+++ src/main/java/com/github/billnduka/Main.java
@@ -13,38 +13,72 @@ import javafx.scene.text.Text;
 import javafx.scene.text.Font;
 
 
-public class Main extends Application { 
+public class Main extends Application 
+{ 
     private static final double WIDTH = 500.0;
     private static final double HEIGHT = 500.0;
+    static final double initX = WIDTH / 2; 
+    static final double initY = HEIGHT - 15;
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        
-
+    public void start(Stage primaryStage) throws Exception 
+    {
         Pane root = new Pane();
-        Scene scene = new Scene(root, WIDTH, HEIGHT); 
+        Scene scene = new Scene(root, WIDTH * 1.1, HEIGHT * 1.1); 
         Image icon = new Image(getClass().getResourceAsStream("/image.png"));
         Canvas canvas = new Canvas(HEIGHT, WIDTH);
-        GraphicsContext context = canvas.getGraphicsContext2D();
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        Ball ball = new Ball(initX, initY, Color.WHITE, new double[] {1, 3});
 
-        context.setFill(Color.BISQUE);
-        context.fillRect(WIDTH / 2 - 40, HEIGHT - 30, 80, 10);
 
-        primaryStage.getIcons().add(icon);
+        gc.setFill(Color.BISQUE);
+        gc.fillRect(0, 0, WIDTH, HEIGHT);
+        ball.drawBall(gc);
+
+
+        
         root.getChildren().add(canvas);
-        canvas.relocate(0, 0);
 
-        scene.setFill(Color.BLACK);
-        context.setFill(Color.BISQUE);
-        context.fillRect(WIDTH / 2 - 40, HEIGHT - 20, 80, 10);
+        
+        
+        
+        primaryStage.getIcons().add(icon);
         primaryStage.setTitle("Brick Breaker");
         primaryStage.setScene(scene);
-
         primaryStage.show();
+    }
+
+    private static class Ball
+    {
+        static final double radius = 15.0;
+        double xPos, yPos;
+        Color ballColor;
+        double[] velocity = {0, 0};
+
+        public Ball(double x, double y, Color color, double[] veloc)
+        {
+            xPos = x;
+            yPos = y; 
+            ballColor = color;
+            velocity = veloc;
+        }
+
+        private void drawBall(GraphicsContext gc)
+        {
+            gc.setStroke(this.ballColor);
+            gc.setLineWidth(3);
+            gc.strokeOval(this.xPos, this.yPos, Ball.radius, Ball.radius);
+        }
+
+        private void updateBall()
+        {
+
+        }
         
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         launch(args); // Use launch() to start the JavaFX application
     }
 }
